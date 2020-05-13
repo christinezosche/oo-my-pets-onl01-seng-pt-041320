20091f1f9926b2072ed41ac7c9c1036bebd22a5f
Christine Zosche <christine.zosche@gmail.com>
2020-05-13 15:25:51 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -34,9 +34,8 @@ class Owner
     Dog.all.select{|dog| dog.owner == self}
   end
   
-  def buy_cat(cat)
-    cat = Cat.new
-    cat.owner = self
+  def buy_cat
+    Cat.new(name, self)
   end
 
 end
